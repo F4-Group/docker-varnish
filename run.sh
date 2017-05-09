ce17339ce1d37a83979d6f48663f1e88f1e7281b
Charly Koza <cka@f4-group.com>
2017-05-09 11:56:05 +0200

    simplified command

--- run.sh
+++ run.sh
@@ -11,11 +11,10 @@ if [ ! -f /etc/varnish/secret ]; then
     dd if=/dev/random of=/etc/varnish/secret count=1
 fi
 
-exec bash -c \
-  "exec varnishd -F \
+exec varnishd -F \
   -a :80 \
   -T localhost:6082 \
   -S /etc/varnish/secret \
   -f $VCL_CONFIG \
   -s malloc,$CACHE_SIZE \
-  $VARNISHD_PARAMS" 2>&1
+  $VARNISHD_PARAMS
